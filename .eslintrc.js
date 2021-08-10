9f9ae314656ab18b250ba92812b3dffe23eb7f32
Ricardo Carrola <ricardo.carrola@gmail.com>
2021-08-10 16:19:15 +0100

    fix:linter and issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
+    jest: true,
+    browser:true,
+    node:true,
     es2021: true,
+  },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
